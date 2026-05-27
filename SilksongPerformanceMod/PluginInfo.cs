2daa9e60faa6d71e406a98eb4f73ccbe6d971bf0
JuJu <ajudagamesss@gmail.com>
2026-05-27 17:51:10 -0300

    Delete SilksongPerformanceMod directory

--- SilksongPerformanceMod/PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace SilksongPerformanceMod;
-
-internal static class PluginInfo
-{
-    public const string Guid = "juliano.performance";
-    public const string Name = "Performance";
-    public const string Version = "0.2.5";
-}
